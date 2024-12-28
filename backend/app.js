f9ab7a55a294735373e93c5846e38d91763afd9f
Fiyinfoluwa Afolayan <fiyinfoluwa.afolayan@gmail.com>
2024-12-28 01:17:52 -0600

    setup postgresql db and prisma client

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+const prisma = require('@prisma/client');
 require('dotenv').config();
 
 const app = express();
+const { PrismaClient } = prisma;
+const prismaClient = new PrismaClient();
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Routes
 app.use('/', require('./routes/index'));
