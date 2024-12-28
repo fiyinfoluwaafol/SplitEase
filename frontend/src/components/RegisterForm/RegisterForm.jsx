4ffc5d8b10604195639bc5a652cf56945aee1294
Fiyinfoluwa Afolayan <fiyinfoluwa.afolayan@gmail.com>
2024-12-28 15:48:08 -0600

    register form works on frontend alone

--- frontend/src/components/RegisterForm/RegisterForm.jsx
+++ frontend/src/components/RegisterForm/RegisterForm.jsx
@@ -1,10 +1,60 @@
 import { useState } from "react";
 import "./RegisterForm.css";
 
-function RegisterForm () {
+function RegisterForm ({ formData, handleInputChange }) {
+
     return (
         <div>
-            Registration Form
+            <form>
+                <div id="name-container">
+                    <div>
+                        <p>First Name</p>
+                        <input
+                            name="firstName"
+                            type="text"
+                            value={formData.firstName}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div>
+                        <p>Last Name</p>
+                        <input
+                            name="lastName"   
+                            type="text"
+                            value={formData.lastName}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                </div>
+
+                <p>Email Address</p>
+                <input
+                    name="email"
+                    type="email"
+                    placeholder="johndoe@email.com"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                />
+
+                <p>Password</p>
+                <div className="password-div">
+                    <input
+                        name="password"
+                        type="password"
+                        value={formData.password}
+                        onChange={handleInputChange}
+                    />
+                    <p id="show-hide-bttn">Show</p>
+                </div>
+                <button>Register</button>
+            </form>
+
+            <p>Or Continue With</p>
+            <div id="social-icons">
+                <button>Google</button>
+                <button>Apple</button>
+                <button>Microsoft</button>
+            </div>
         </div>
     )
 }
--- frontend/src/pages/UserAuthPage/UserAuthPage.jsx
+++ frontend/src/pages/UserAuthPage/UserAuthPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./UserAuthPage.css"
 // TODO: import LoginForm component
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
@@ -5,6 +6,21 @@ import RegisterForm from "../../components/RegisterForm/RegisterForm";
 // TODO: import logo and graphic for right half
 
 function UserAuthPage () {
+const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+})
+
+const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
     return (
         <>
             <div id="entire-page-container">
@@ -16,7 +32,10 @@ function UserAuthPage () {
                     <div>
                         <button>Register or Login Toggle Button</button>
                     </div>
-                    <RegisterForm />
+                    <RegisterForm 
+                        formData={formData}
+                        handleInputChange={handleInputChange}
+                    />
                 </div>
                 <div id="img-container">
                     <img alt="graphic of SplitEase"/>
